8907352c860b83d671beaaed733bf31da894a3c5
SebasAC22 <sebastian40703@gmail.com>
2026-03-19 22:59:40 -0600

    Agregue elementos en el Form para probar la BD

--- Quiz/Preguntas.Designer.cs
+++ Quiz/Preguntas.Designer.cs
@@ -28,20 +28,168 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.SuspendLayout();
-			// 
-			// Preguntas
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Name = "Preguntas";
-			this.Text = "Preguntas";
-			this.Load += new System.EventHandler(this.Preguntas_Load);
-			this.ResumeLayout(false);
+            this.lblPregunta = new System.Windows.Forms.Label();
+            this.pnlOpcion1 = new System.Windows.Forms.Panel();
+            this.pnlOpcion2 = new System.Windows.Forms.Panel();
+            this.pnlOpcion3 = new System.Windows.Forms.Panel();
+            this.pnlOpcion4 = new System.Windows.Forms.Panel();
+            this.lblOpcion1 = new System.Windows.Forms.Label();
+            this.lblOpcion2 = new System.Windows.Forms.Label();
+            this.lblOpcion3 = new System.Windows.Forms.Label();
+            this.lblOpcion4 = new System.Windows.Forms.Label();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.lblPuntaje = new System.Windows.Forms.Label();
+            this.pnlOpcion1.SuspendLayout();
+            this.pnlOpcion2.SuspendLayout();
+            this.pnlOpcion3.SuspendLayout();
+            this.pnlOpcion4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblPregunta
+            // 
+            this.lblPregunta.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPregunta.Location = new System.Drawing.Point(23, 36);
+            this.lblPregunta.Name = "lblPregunta";
+            this.lblPregunta.Size = new System.Drawing.Size(423, 23);
+            this.lblPregunta.TabIndex = 0;
+            this.lblPregunta.Text = "label1";
+            this.lblPregunta.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnlOpcion1
+            // 
+            this.pnlOpcion1.Controls.Add(this.lblOpcion1);
+            this.pnlOpcion1.Location = new System.Drawing.Point(28, 86);
+            this.pnlOpcion1.Name = "pnlOpcion1";
+            this.pnlOpcion1.Size = new System.Drawing.Size(200, 100);
+            this.pnlOpcion1.TabIndex = 1;
+            // 
+            // pnlOpcion2
+            // 
+            this.pnlOpcion2.Controls.Add(this.lblOpcion2);
+            this.pnlOpcion2.Location = new System.Drawing.Point(246, 86);
+            this.pnlOpcion2.Name = "pnlOpcion2";
+            this.pnlOpcion2.Size = new System.Drawing.Size(200, 100);
+            this.pnlOpcion2.TabIndex = 2;
+            // 
+            // pnlOpcion3
+            // 
+            this.pnlOpcion3.Controls.Add(this.lblOpcion3);
+            this.pnlOpcion3.Location = new System.Drawing.Point(28, 203);
+            this.pnlOpcion3.Name = "pnlOpcion3";
+            this.pnlOpcion3.Size = new System.Drawing.Size(200, 100);
+            this.pnlOpcion3.TabIndex = 3;
+            // 
+            // pnlOpcion4
+            // 
+            this.pnlOpcion4.Controls.Add(this.lblOpcion4);
+            this.pnlOpcion4.Location = new System.Drawing.Point(246, 203);
+            this.pnlOpcion4.Name = "pnlOpcion4";
+            this.pnlOpcion4.Size = new System.Drawing.Size(200, 100);
+            this.pnlOpcion4.TabIndex = 4;
+            // 
+            // lblOpcion1
+            // 
+            this.lblOpcion1.AutoSize = true;
+            this.lblOpcion1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblOpcion1.Location = new System.Drawing.Point(0, 0);
+            this.lblOpcion1.Name = "lblOpcion1";
+            this.lblOpcion1.Size = new System.Drawing.Size(35, 13);
+            this.lblOpcion1.TabIndex = 0;
+            this.lblOpcion1.Text = "label1";
+            this.lblOpcion1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOpcion2
+            // 
+            this.lblOpcion2.AutoSize = true;
+            this.lblOpcion2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblOpcion2.Location = new System.Drawing.Point(0, 0);
+            this.lblOpcion2.Name = "lblOpcion2";
+            this.lblOpcion2.Size = new System.Drawing.Size(35, 13);
+            this.lblOpcion2.TabIndex = 0;
+            this.lblOpcion2.Text = "label2";
+            this.lblOpcion2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOpcion3
+            // 
+            this.lblOpcion3.AutoSize = true;
+            this.lblOpcion3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblOpcion3.Location = new System.Drawing.Point(0, 0);
+            this.lblOpcion3.Name = "lblOpcion3";
+            this.lblOpcion3.Size = new System.Drawing.Size(35, 13);
+            this.lblOpcion3.TabIndex = 0;
+            this.lblOpcion3.Text = "label3";
+            this.lblOpcion3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOpcion4
+            // 
+            this.lblOpcion4.AutoSize = true;
+            this.lblOpcion4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblOpcion4.Location = new System.Drawing.Point(0, 0);
+            this.lblOpcion4.Name = "lblOpcion4";
+            this.lblOpcion4.Size = new System.Drawing.Size(35, 13);
+            this.lblOpcion4.TabIndex = 0;
+            this.lblOpcion4.Text = "label4";
+            this.lblOpcion4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(468, 45);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(75, 13);
+            this.lblNumero.TabIndex = 5;
+            this.lblNumero.Text = "label Pregunta";
+            // 
+            // lblPuntaje
+            // 
+            this.lblPuntaje.AutoSize = true;
+            this.lblPuntaje.Location = new System.Drawing.Point(448, 319);
+            this.lblPuntaje.Name = "lblPuntaje";
+            this.lblPuntaje.Size = new System.Drawing.Size(74, 13);
+            this.lblPuntaje.TabIndex = 6;
+            this.lblPuntaje.Text = "label Puntuaje";
+            // 
+            // Preguntas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.lblPuntaje);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.pnlOpcion4);
+            this.Controls.Add(this.pnlOpcion3);
+            this.Controls.Add(this.pnlOpcion2);
+            this.Controls.Add(this.pnlOpcion1);
+            this.Controls.Add(this.lblPregunta);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Preguntas";
+            this.Text = "Preguntas";
+            this.Load += new System.EventHandler(this.Preguntas_Load);
+            this.pnlOpcion1.ResumeLayout(false);
+            this.pnlOpcion1.PerformLayout();
+            this.pnlOpcion2.ResumeLayout(false);
+            this.pnlOpcion2.PerformLayout();
+            this.pnlOpcion3.ResumeLayout(false);
+            this.pnlOpcion3.PerformLayout();
+            this.pnlOpcion4.ResumeLayout(false);
+            this.pnlOpcion4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private System.Windows.Forms.Label lblPregunta;
+        private System.Windows.Forms.Panel pnlOpcion1;
+        private System.Windows.Forms.Label lblOpcion1;
+        private System.Windows.Forms.Panel pnlOpcion2;
+        private System.Windows.Forms.Label lblOpcion2;
+        private System.Windows.Forms.Panel pnlOpcion3;
+        private System.Windows.Forms.Label lblOpcion3;
+        private System.Windows.Forms.Panel pnlOpcion4;
+        private System.Windows.Forms.Label lblOpcion4;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Label lblPuntaje;
+    }
 }
